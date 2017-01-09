b8d395dcd58d32b0e0197aac3594275d6651f0e2
dkonate <dotemin@gmail.com>
2017-01-09 09:36:09 +0100

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,56 @@
+<header>
+		<div class="w3layouts-top-strip">
+			<div class="container-fluid">
+				<div class="w3ls-social-icons">
+					<a class="facebook" href="#"><i class="fa fa-facebook"></i></a>
+					<a class="twitter" href="#"><i class="fa fa-twitter"></i></a>
+					<a class="pinterest" href="#"><i class="fa fa-pinterest-p"></i></a>
+					<a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a>
+					<a class="tumblr" href="#"><i class="fa fa-tumblr"></i></a>
+				</div>
+				<div class="agileits-contact-info text-right">
+					<ul>
+						<li><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>+0033 6 78 57 92 35</li>
+						<li><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span><a href="mailto:info@juststartupchallenge.fr">info@juststartupchallenge.fr</a></li>
+					</ul>
+				</div>
+				<div class="logo text-center">
+					<h1><a href="index.php"> Just Startup Challenge </a></h1>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</header>
+	<!-- //header -->
+		<div class="w3layouts-inner-banner">
+		</div>
+	</div>
+	<div class="clearfix"></div>
+	<!-- navigation -->
+			<nav class="navbar navbar-default">
+			  <div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+				  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+
+				  </button>
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li><a class="active" href="index.php">Accueil</a></li>
+						<li><a href="aproposconcours.php">A Propos du concours</a></li>
+						<li><a href="portfolio.html">Organisateurs</a></li>
+						<li><a href="blog.html">Sponsors</a></li>
+						<li><a href="contact.html">Inscription</a></li>
+						<li><a href="contact.html">Contact</a></li>
+						<li><a href="#">FAQ</a></li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			  </div><!-- /.container-fluid -->
+			</nav>
---
+++ index_ancien.php
@@ -0,0 +1,486 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<title>Just Startup challenge</title>
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all" /><!-- fontawesome -->
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all" /><!-- Bootstrap stylesheet -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" /><!-- stylesheet -->
+<!-- meta tags -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Business Strategy Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //meta tags -->
+<!--fonts-->
+<link href="//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i&subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Philosopher" rel="stylesheet">
+<!--//fonts-->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script><!-- Required-js -->
+<script src="js/bootstrap.min.js"></script><!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<!-- main slider-banner -->
+<script src="js/skdslider.min.js"></script>
+<link href="css/skdslider.css" rel="stylesheet">
+<script type="text/javascript">
+		jQuery(document).ready(function(){
+			jQuery('#demo1').skdslider({'delay':5000, 'animationSpeed': 2000,'showNextPrev':true,'showPlayButton':true,'autoSlide':true,'animationType':'fading'});
+
+			jQuery('#responsive').change(function(){
+			  $('#responsive_wrapper').width(jQuery(this).val());
+			});
+
+		});
+</script>
+<!-- //main slider-banner -->
+<!-- scriptfor smooth drop down-nav -->
+<script>
+$(document).ready(function(){
+    $(".dropdown").hover(
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');
+        },
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');
+        }
+    );
+});
+</script>
+<!-- //script for smooth drop down-nav -->
+</head>
+<body>
+	<!-- top-header and slider -->
+	<div class="w3-slider">
+	<!-- header -->
+	<header>
+		<div class="w3layouts-top-strip">
+			<div class="container-fluid">
+				<div class="w3ls-social-icons">
+					<a class="facebook" href="#"><i class="fa fa-facebook"></i></a>
+					<a class="twitter" href="#"><i class="fa fa-twitter"></i></a>
+					<a class="pinterest" href="#"><i class="fa fa-pinterest-p"></i></a>
+					<a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a>
+					<a class="tumblr" href="#"><i class="fa fa-tumblr"></i></a>
+				</div>
+				<div class="agileits-contact-info text-right">
+					<ul>
+						<li><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>+33 6 78 57 92 35</li>
+						<li><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span><a href="mailto:info@juststartupchallenge.fr">info@juststartupchallenge.fr</a></li>
+					</ul>
+				</div>
+				<div class="logo text-center">
+					<h1><a href="index.php">Just Startup Challenge</a></h1>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</header>
+	<!-- //header -->
+	<!-- main-slider -->
+		<ul id="demo1">
+			<li>
+			<img src="images/1.jpg" alt="" />
+			<!--Slider Description example-->
+			 <div class="slide-desc">
+					<h3>JUST DO IT!</h3>
+			  </div>
+			</li>
+			<li>
+				<img src="images/2.jpg" alt="" />
+			</li>
+			<li>
+				<img src="images/3.jpg" alt="" />
+			</li>
+			<li>
+				<img src="images/4.jpg" alt="" />
+			</li>
+			<li>
+				<img src="images/5.jpg" alt="" />
+			</li>
+		</ul>
+	</div>
+	<!-- //main-slider -->
+	<!-- //top-header and slider -->
+	<!-- navigation -->
+			<nav class="navbar navbar-default">
+			  <div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+				  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				  </button>
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				  <ul class="nav navbar-nav">
+					<li><a class="active" href="index.php">Accueil</a></li>
+					<li><a href="#aproposconcours">A Propos du concours</a></li>
+					<li><a href="#objectifs">Nos objectifs</a></li>
+					<li><a href="#planning">Planning</a></li>
+					<li><a href="inscription.php">Inscription</a></li>
+					<li><a href="#contacts">Contact</a></li>
+				  </ul>
+				</div><!-- /.navbar-collapse -->
+			  </div><!-- /.container-fluid -->
+			</nav>
+		<!-- //navigation -->
+		<!-- main-content -->
+		<div class="wthree-main-content">
+
+
+			<section class="w3-about text-center aproposconcours" id="aproposconcours">
+
+				<div class="container">
+					<h2 class="agile-inner-title">A propos du concours</h2>
+					<div class="service-section" id="service">
+						<div class="w3ls-service-header">
+							<p>Just-startup-challenge est un concour de création d'entreprise. Le but est d'élire par un jury de professionnels, la startup ayant la meilleure idée, la meilleure réalisation et la meilleure stratégie. </p>
+						</div>
+					</div>
+				</div>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6 text-left">
+							<h2>Just Startup Challenge </h2>
+							<h3 class="caption gray">C'est un concours créé en 2008</h3>
+							<p>Le concours se déroule à porte de Versaille, il se déroule sur un an et à pour but d'élire la meilleure jeune entreprise se présentant au concours. Trois critéres sont pris en considération pour élire la meilleure entrerpsie à savoir : La créativité et l'idée de l'entreprise,    </p>
+
+							<blockquote>
+
+								<span>Ray Goforth</span>
+							</blockquote>
+
+							<h3 class="gray light">Over the past 11 years we've worked with some amazing small businesses, non-profits, academic institutions and even Fortune 100 companies.</h3>
+
+						</div><!-- /.col-md-6 -->
+
+						<div class="col-md-6">
+							<div class="embed-responsive embed-responsive-16by9">
+								<iframe src="https://player.vimeo.com/video/112951662" width="640" height="360" allowfullscreen></iframe>
+							</div>
+						</div><!-- /.col-md-6 -->
+					</div><!-- /.row -->
+				</div><!-- /.container -->
+
+
+
+
+			</section>
+			<!-- Specialize-section -->
+			<section class="w3-about text-center objectifs" id="objectifs">
+				<div class="container">
+					<h3 class="w3l-title">Nos Objectifs</h3>
+					<p class="para">Just Startup challenge à pour ambitions d'aider, d'encourager de jeunes entrepreneur. Ce concours a pour vocation d'accompagner des entrepreneurs en ayant un regard critique. Nous voulons également permettre à nos jeunes entrepreneurs d'exploiter le réseau de son concours pour se faire plus connaitre.
+						Just Startup challenge fixe 3 objectifs majeurs aux jeunes entreprises qui nous servirons de critéres de notation:<span> Il est important de respeecter au mieux ces trois critéres:</span> </p>
+					<div class="col-md-4 w3l-abt-grid">
+						<div class="hi-icon-wrap hi-icon-effect-7 hi-icon-effect-7b">
+							<a href="#" class="hi-icon icon1"><i class="fa fa-graduation-cap" aria-hidden="true"></i></a>
+						</div>
+						<h4>Apprendre</h4>
+						<p>Apprendre à créer et gérer sa mini entreprise en faisant des levées de fonds, des business plans, ainsi qu'en faisant des stratégies marketing.</p>
+					</div>
+					<div class="col-md-4 w3l-abt-grid">
+						<div class="hi-icon-wrap hi-icon-effect-7 hi-icon-effect-7b">
+							<a href="#" class="hi-icon icon2"><i class="fa fa-connectdevelop" aria-hidden="true"></i></a>
+						</div>
+						<h4>Réseau</h4>
+						<p>Se crééer un réseau en exposant son idée lors du salon des minis entreprises et allant démarché des sponsoors </p>
+					</div>
+					<div class="col-md-4 w3l-abt-grid">
+						<div class="hi-icon-wrap hi-icon-effect-7 hi-icon-effect-7b">
+							<a href="#" class="hi-icon icon3"><i class="fa fa-lightbulb-o" aria-hidden="true"></i></a>
+						</div>
+						<h4>Créativité</h4>
+						<p>Trouver la bonne idée, il n'y a pas de bonne ou de mauvaise idée!</p>
+					</div>
+
+					<div class="clearfix"></div>
+				</div>
+			</section>
+			<!-- //Specialize-section -->
+
+			<!-- NEWS -->
+			<section class="wthree-news text-center planning" id="planning">
+			<div class="wthree-news text-center">
+				<div class="container">
+					<h3 class="w3l-title">Notre Planning</h3>
+					<p class="para">Voici les dates importantes qui ont été définis dans le cadre du concours.</p>
+					<div class="col-md-6 wthree-right">
+						<div class="w3-agile-left">
+							<div class="work-left">
+								<h4>22 juillet 2017</h4>
+							</div>
+							<div class="agileits-right text-left">
+								<a>Date limite de dépot de candidature</a>
+								<p>Pour les candidats souhaitant s'inscrire à ce concours, il sera impératif d'envoyer son formulaire d'inscriptions avant le 22 Juillet 2017.</p>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+						<div class="wel-right">
+							<div class="content-item item-image1">
+								<a ><img src="images/n1.jpg" alt="" /></a>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="col-md-6 wthree-right no-marg">
+						<div class="wel-right2">
+							<div class="content-item item-image2">
+								<a ><img src="images/n2.jpg" alt="" /></a>
+							</div>
+						</div>
+						<div class="w3-agile-left2">
+							<div class="work-left">
+								<h4>02 Aout 2017</h4>
+							</div>
+							<div class="agileits-right text-left">
+								<a >Date limite de validation de l'incription</a>
+								<p>Les candidats ayant remplis le formulaire d'inscription dans les temps, receveron un email pour savoir si leur candidature est retenue ou non.</p>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="clearfix"></div>
+					<div class="col-md-6 wthree-right">
+						<div class="w3-agile-left">
+							<div class="work-left">
+								<h4>12 Sep</h4>
+							</div>
+							<div class="agileits-right text-left">
+								<a >Présentation de l'entreprise </a>
+								<p>Les candidats participants au concours devront se présenter une premiére fois devant un jury composé de professionnelles. Tout support sera autrisé pour présenté son entreprise.</p>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+						<div class="wel-right ">
+							<div class="content-item item-image1">
+								<a ><img src="images/n3.jpg" alt="" /></a>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="col-md-6 wthree-right no-marg">
+						<div class="wel-right2">
+							<div class="content-item item-image2">
+								<a ><img src="images/n4.jpg" alt="" /></a>
+							</div>
+						</div>
+						<div class="w3-agile-left2">
+							<div class="work-left">
+								<h4>15 Novembre 2017</h4>
+							</div>
+							<div class="agileits-right text-left">
+								<a >Dépot du dossier </a>
+								<p>Date limite du dossier final qui devra comprendre un business plan,  .</p>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+			<!-- //NEWS -->
+			</section>
+
+			<!--clients-->
+			<div class="w3layouts-clients">
+				<div class="container">
+					<h3>Nos Sponsors</h3>
+					<div class="flex-slider">
+						<ul id="flexiselDemo1">
+							<li>
+								<img src="images/c1.png" alt=""/>
+							</li>
+							<li>
+								<img src="images/c2.png" alt=""/>
+							</li>
+							<li>
+								<img src="images/c3.png" alt=""/>
+							</li>
+							<li>
+								<img src="images/c4.png" alt=""/>
+							</li>
+						</ul>
+						<script type="text/javascript">
+										$(window).load(function() {
+											$("#flexiselDemo1").flexisel({
+												visibleItems: 4,
+												animationSpeed: 1000,
+												autoPlay: true,
+												autoPlaySpeed: 3000,
+												pauseOnHover: true,
+												enableResponsiveBreakpoints: true,
+												responsiveBreakpoints: {
+													portrait: {
+														changePoint:414,
+														visibleItems: 1
+													},
+													landscape: {
+														changePoint:667,
+														visibleItems: 2
+													},
+													tablet: {
+														changePoint:991,
+														visibleItems: 3
+													}
+												}
+											});
+
+										});
+						</script>
+						<script type="text/javascript" src="js/jquery.flexisel.js"></script>
+					</div>
+				</div>
+			</div>
+			<!--//clients-->
+
+		</div>
+		<!-- //main-content -->
+		<!-- contact us -->
+	<section class="agileits-contactus contacts" id="contacts">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div>
+					<h3 class="w3l-title">Nous contacter</h3>
+					</div>
+					<div class="col-sm-8 w3layouts-agileits">
+						<form action="contact-action.php"  method="post" class="my-form">
+						  <div class="form-group">
+							<input type="text" class="form-control" name="name" id="name" placeholder="Nom" required="required">
+						  </div>
+						  <div class="form-group">
+							<input type="email" class="form-control" name="email" id="email" placeholder="Email" required="required">
+						  </div>
+						  <div class="form-group">
+							<textarea class="form-control" name="message" id="message" rows="7" cols="25" style="resize:none;" required="required" placeholder="Message"></textarea>
+						  </div>
+
+						  <button type="submit" class="btn btn-default btn-block">ENVOYER MESSAGE</button>
+						</form>
+					</div>
+
+					<div class="col-sm-4 w3layouts-agileits">
+						<div class="w3layouts-address">
+						<!--<h4>Main Office</h4>-->
+
+						<address>
+						  <strong class="bold">Just Startup Challenge</strong><br>
+						  15-RG, Lorem Ipsum,<br>
+						  Main Road, quibusdam et aut<br>
+						  <abbr title="Phone">Mobile : </abbr> +33 6 78 57 92 35<br>
+						   <abbr title="Phone">Phone : </abbr> +33 6 78 57 92 35
+						</address>
+
+						<address>
+						  <strong class="bold">Email</strong><br>
+						  <a class="mail" href="mailto:info@juststartupchallenge.fr">info@juststartupchallenge.fr</a>
+						</address>
+						<address>
+						  <strong class="bold">We're on social networks</strong><br>
+							<div>
+							<ul class="agileits-company-social">
+								<li class="social-facebook"><a href="#" target="_blank" title="facebook"><i class="fa fa-facebook"></i></a></li>
+								<li class="social-twitter"><a href="#" target="_blank" title="twitter"><i class="fa fa-twitter"></i></a></li>
+								<li class="social-dribble"><a href="#" target="_blank" title="dribble"><i class="fa fa-dribbble"></i></a></li>
+								<li class="social-google"><a href="#" target="_blank" title="google"><i class="fa fa-google-plus"></i></a></li>
+							</ul>
+							</div>
+						</address>
+						</div>
+					</div>
+				</div>
+
+				</div>
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<br>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1312.6818946796031!2d2.287456410311682!3d48.851272950865074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6701b487a1783%3A0x79f04230918a64c6!2s10+Rue+Sextius+Michel%2C+75015+Paris!5e0!3m2!1sfr!2sfr!4v1483488811043" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
+			</div>
+
+
+		</div>
+	</section>
+	<!-- /.contact us -->
+	<!-- footer -->
+		<footer>
+			<div class="container">
+				<div class="w3ls-footer-grids">
+					<div class="col-md-3 w3l-footer-grid">
+						<h4>NOTRE ADRESSE</h4>
+						<ul>
+							<li>BS Business Strategy</li>
+							<li>4111 Deer Haven Drive Greenville, SC 29601 </li>
+							<li>Hours: Mon-Fri 9am - 6:00pm</li>
+						</ul>
+					</div>
+					<div class="col-md-3 w3l-footer-grid">
+						<h4>NOUS CONTACTER</h4>
+						<ul>
+							<li>Tel: +33 6 78 57 92 35</li>
+							<li>Fax: +33 6 78 57 92 35</li>
+							<li>Email: <a href="mailto:info@juststartupchallenge.fr">info@juststartupchallenge.fr </a></li>
+						</ul>
+					</div>
+					<div class="col-md-3 w3l-footer-grid">
+						<h4>NEWSLETTER</h4>
+						<form action="#" method="post">
+							<input type="email" name="email" placeholder="Entrer votre email" required="required">
+							<input type="submit" value=" ">
+						</form>
+					</div>
+					<div class="col-md-3 w3l-footer-grid">
+						<h4>Copy Rights</h4>
+						<p>&copy; 2016 Just Startup Challenge <a href=""> W3layouts</a></p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</footer>
+		<!-- //footer -->
+	<!-- here stars scrolling icon -->
+			<script type="text/javascript">
+				$(document).ready(function() {
+					/*
+						var defaults = {
+						containerID: 'toTop', // fading element id
+						containerHoverID: 'toTopHover', // fading element hover id
+						scrollSpeed: 1200,
+						easingType: 'linear'
+						};
+					*/
+
+					$().UItoTop({ easingType: 'easeOutQuart' });
+
+					});
+			</script>
+			<!-- start-smoth-scrolling -->
+			<script type="text/javascript" src="js/move-top.js"></script>
+			<script type="text/javascript" src="js/easing.js"></script>
+			<script type="text/javascript">
+				jQuery(document).ready(function($) {
+					$(".scroll").click(function(event){
+						event.preventDefault();
+						$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+					});
+				});
+			</script>
+			<!-- start-smoth-scrolling -->
+		<!-- //here ends scrolling icon -->
+</body>
+</html>
